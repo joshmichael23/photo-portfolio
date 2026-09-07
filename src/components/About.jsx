174a7f7dd526f0e0b4b1b9619eec7ae737866fd5
josholea <olea.josh@gmail.com>
2026-09-08 03:23:36 +0800

    update about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import image from '../assets/IMG_2304.jpg'
 const About = () => {
   return (
     <div id="about"className="about-section">
         <h2>About</h2>
-        <img src="src\assets\IMG_2304.jpg" alt="Your Name" />
+        <img src={image} alt="Your Name" />
         <p>I’m a photographer drawn to honest moments, quiet details, and the stories found in everyday life.</p>
         <p className="location">Location</p>
         <p>Naga City, Camarines Sur</p>
